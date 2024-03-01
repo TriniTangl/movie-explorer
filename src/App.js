e70dd9f04ca28e3f7b919fdae9589483d59d3d9c
Vladyslav Havryliuk <vladyslavhavryliuk@Vladyslavs-MacBook-Pro.local>
2024-03-01 15:06:19 +0200

    set up initial components and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MovieDetails from './containers/MovieDetails';
+import PopularMovies from './containers/PopularMovies';
+
 function App() {
-  return ('');
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <PopularMovies /> } />
+        <Route path="/movie/:id" element={ <MovieDetails /> } />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
