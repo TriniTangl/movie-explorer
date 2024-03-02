e2c784eb8ee8b5cbe571c133cc0656f783e90ced
Vladyslav Havryliuk <vladyslavhavryliuk@Vladyslavs-MacBook-Pro.local>
2024-03-03 01:22:38 +0200

    add list with searched results to Suggestion component

--- src/components/Suggestion.js
+++ src/components/Suggestion.js
@@ -1,17 +1,129 @@
-import { TextField } from '@mui/material';
+import { Grid, MenuItem, Paper, styled, TextField, Typography } from '@mui/material';
+import Downshift from 'downshift';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { COVER_PLACEHOLDER, IMAGES_PATH } from '../config';
+import { searchMovies } from '../redux/search';
 
-const Suggestion = () => {
+const PaperStyled = styled(Paper)({
+  backgroundColor: 'darkgoldenrod',
+  top: -40,
+  position: 'relative',
+});
+
+const MenuItemStyled = styled(MenuItem)({
+  paddingTop: 5,
+  paddingBottom: 5,
+});
+
+const ImgStyled = styled('img')({
+  height: '100%',
+});
+
+const LinkStyled = styled(Link)({
+  display: 'block',
+  textDecoration: 'none',
+});
+
+const Suggestion = ({ movies }) => {
+  const dispatch = useDispatch();
+
+  const inputOnChange = (event) => {
+    if (!event.target.value) {
+      return;
+    }
+
+    dispatch(searchMovies(event.target.value));
+  };
+
   return (
-    <>
-      <TextField
-        id="search"
-        placeholder="Search"
-        fullWidth={ true }
-        sx={ { mb: 5 } }
-        variant="standard"
-      />
-    </>
+    <Downshift>
+      { ({
+           getInputProps,
+           getItemProps,
+           getMenuProps,
+           isOpen,
+           inputValue,
+           highlightedIndex,
+           selectedItem,
+         }) => (
+        <div>
+          <TextField
+            id="search"
+            placeholder="Search"
+            fullWidth={ true }
+            sx={ { mb: 5 } }
+            variant="standard"
+            InputProps={ {
+              ...getInputProps({
+                onChange: inputOnChange,
+              }),
+            } }
+          />
+          {
+            isOpen
+              ? (
+                <PaperStyled
+                  { ...getMenuProps() }
+                  square={ true }
+                >
+                  {
+                    movies.results
+                      .slice(0, 10)
+                      .filter(
+                        (item) => !inputValue || item.title.toLowerCase().includes(inputValue.toLowerCase()),
+                      )
+                      .map(
+                        (item, index) => (
+                          <MenuItemStyled { ...getItemProps({
+                            item,
+                            key: item.id,
+                            selected: highlightedIndex === index,
+                            style: {
+                              fontWeight: selectedItem === item ? 500 : 400,
+                            },
+                          }) }>
+                            <LinkStyled to={ `/movie/${ item.id }` }>
+                              <Grid container={ true } spacing={ 8 }>
+                                <Grid item={ true }>
+                                  {
+                                    item.poster_path
+                                      ? (
+                                        <ImgStyled
+                                          src={ `${ IMAGES_PATH }/w92${ item.poster_path }` }
+                                          alt={ item.title }
+                                        />
+                                      )
+                                      : (
+                                        <ImgStyled
+                                          src={ COVER_PLACEHOLDER }
+                                          alt={ item.title }
+                                        />
+                                      )
+                                  }
+                                </Grid>
+                                <Grid item={ true }>
+                                  <Typography variant="h4">
+                                    { item.title }
+                                  </Typography>
+                                  <Typography variant="caption">
+                                    { item.title }
+                                  </Typography>
+                                </Grid>
+                              </Grid>
+                            </LinkStyled>
+                          </MenuItemStyled>
+                        ),
+                      )
+                  }
+                </PaperStyled>
+              )
+              : null
+          }
+        </div>
+      ) }
+    </Downshift>
   );
 };
 
--- src/containers/SearchMovieSuggestion.js
+++ src/containers/SearchMovieSuggestion.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Suggestion from '../components/Suggestion';
 
 const SearchMovieSuggestion = () => {
-  return (<Suggestion />);
+  const { search } = useSelector((store) => store);
+
+  return (<Suggestion movies={ search } />);
 };
 
 export default SearchMovieSuggestion;
