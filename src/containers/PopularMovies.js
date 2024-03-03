b3bb3fc86314b6e6d712fdbc7ce7350e0c8c9741
Vladyslav Havryliuk <vladyslavhavryliuk@Vladyslavs-MacBook-Pro.local>
2024-03-04 00:24:58 +0200

    add Movies component to the PopularMovies component on the main page

--- src/containers/PopularMovies.js
+++ src/containers/PopularMovies.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import { Typography } from '@mui/material';
+import React, { useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../components/Loader';
+import Movies from '../components/Movies';
+import { getPopularMovies, resetState } from '../redux/movies';
 
 const PopularMovies = () => {
-  return ('Hello from Popular Movies');
+  const dispatch = useDispatch();
+  const { movies } = useSelector((store) => store);
+  const { genres } = useSelector((store) => store.genres);
+
+  useEffect(() => {
+    dispatch(getPopularMovies());
+
+    return () => {
+      dispatch(resetState());
+    };
+  }, [dispatch]);
+
+  const loadMore = () => {
+    if (movies.hasMore) {
+      dispatch(getPopularMovies(movies.page + 1));
+    }
+  };
+
+  return (
+    movies.page === 0 && movies.isFetching
+      ? <Loader />
+      : <>
+        <Typography component="h2" variant="h3" gutterBottom={ true }>
+          Popular Movies
+        </Typography>
+        <InfiniteScroll
+          dataLength={ movies.totalResults }
+          next={ loadMore }
+          hasMore={ movies.hasMore }
+          loader={ <Loader /> }
+          style={ { overflow: 'hidden' } }
+          endMessage={ <p>Yay! You have seen it all!</p> }
+        >
+          <Movies movies={ movies } genres={ genres } />
+        </InfiniteScroll>
+      </>
+  );
 };
 
 export default PopularMovies;
