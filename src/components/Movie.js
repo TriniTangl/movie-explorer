b164b4903b203b0fc38d7e95f2607674138a90e8
Vladyslav Havryliuk <vladyslavhavryliuk@Vladyslavs-MacBook-Pro.local>
2024-03-04 03:05:52 +0200

    create Movie component and add it to MovieDetails container

---
+++ src/components/Movie.js
@@ -0,0 +1,87 @@
+import { Grid, styled, Typography } from '@mui/material';
+import React from 'react';
+import { COVER_PLACEHOLDER_FOR_MAIN_PAGE, IMAGES_PATH } from '../config';
+import Movies from './Movies';
+
+const GridStyled = styled(Grid)(({ theme }) => ({
+  marginBottom: theme.spacing(3),
+}));
+
+const ImgStyled = styled('img')({
+  width: '100%',
+});
+
+const Movie = ({ movie, recommendations, genres }) => {
+  const getDetailsBlock = (title, info) => {
+    return info
+      ? (
+        <>
+          <Typography component="h3" variant="h6">
+            { title }
+          </Typography>
+          <Typography variant="body1" gutterBottom={ true }>
+            { info }
+          </Typography>
+        </>
+      )
+      : null;
+  };
+
+  const formatRuntime = (runtime = 0) => {
+    runtime = Number(runtime);
+    const hours = Math.floor(runtime / 60) + 'h';
+    const minutes = (runtime % 60) + 'm';
+
+    return runtime ? `${ hours } ${ minutes }` : '';
+  };
+
+  return (
+    <>
+      <GridStyled container={ true } spacing={ 2 }>
+        <Grid item={ true } md={ 3 }>
+          {
+            movie.poster_path
+              ? <ImgStyled
+                src={ `${ IMAGES_PATH }/w300${ movie.poster_path }` }
+                alt={ movie.title }
+              />
+              : <ImgStyled
+                src={ COVER_PLACEHOLDER_FOR_MAIN_PAGE }
+                alt={ movie.title }
+              />
+          }
+        </Grid>
+        <Grid item={ true } md={ 9 }>
+          <Typography component="h1" variant="h3" gutterBottom={ true }>
+            { movie.title }
+          </Typography>
+          { getDetailsBlock('Tagline:', movie.tagline) }
+          { getDetailsBlock('Genres:', movie.genres?.map((genre) => genre.name).join(', ')) }
+          { getDetailsBlock('Country:', movie.production_countries?.map((country) => country.name).join(', ')) }
+          { getDetailsBlock('Duration:', formatRuntime(movie.runtime)) }
+          { getDetailsBlock('Release Date:', new Date(movie.release_date).toLocaleDateString(
+            'en-US',
+            {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            },
+          )) }
+          { getDetailsBlock('Overview:', movie.overview) }
+        </Grid>
+      </GridStyled>
+      {
+        recommendations && recommendations.length && (
+          <>
+            <Typography component="h2" variant="h4" gutterBottom={ true }>
+              Recommended
+            </Typography>
+            <Movies movies={ recommendations } genres={ genres } />
+          </>
+        )
+      }
+    </>
+  );
+};
+
+export default Movie;
--- src/containers/MovieDetails.js
+++ src/containers/MovieDetails.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import Loader from '../components/Loader';
+import Movie from '../components/Movie';
+import { getGenresState } from '../redux/genres';
+import { getMovie, getMovieState, resetState } from '../redux/movie';
 
 const MovieDetails = () => {
-  return ('Hello from Movie Details');
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const movie = useSelector((store) => getMovieState(store).movie);
+  const isFetchingMovie = useSelector((store) => getMovieState(store).isFetching);
+  const recommendations = useSelector((store) => getMovieState(store).recommendations.results);
+  const genres = useSelector((store) => getGenresState(store).genres);
+  const isFetchingGenres = useSelector((store) => getGenresState(store).isFetching);
+
+  useEffect(() => {
+    dispatch(getMovie(id ? parseInt(id, 10) : 0));
+
+    return () => {
+      dispatch(resetState());
+    };
+  }, [dispatch, id]);
+
+  return (
+    isFetchingMovie || isFetchingGenres
+      ? <Loader />
+      : <Movie movie={ movie } recommendations={ recommendations } genres={ genres } />
+  );
 };
 
 export default MovieDetails;
