6e2bc5fbf774e4bd3ebd9e6ea2f7e6e3f8b98142
Vladyslav Havryliuk <vladyslavhavryliuk@Vladyslavs-MacBook-Pro.local>
2024-03-04 03:06:03 +0200

    refactor API file

--- src/api/api.js
+++ src/api/api.js
@@ -7,19 +7,25 @@ export default class TheMovieDbApi {
   }
 
   searchMovies = async (query) => {
-    const response = await fetch(`${ this.apiBaseUrl }/search/movie?api_key=${ this.apiKey }&query=${ query }`);
+    const response = await fetch(
+      `${ this.apiBaseUrl }/search/movie?api_key=${ this.apiKey }&query=${ query }`,
+    );
 
     return response.json();
   };
 
   getGenres = async () => {
-    const response = await fetch(`${ this.apiBaseUrl }/genre/movie/list?api_key=${ this.apiKey }`);
+    const response = await fetch(
+      `${ this.apiBaseUrl }/genre/movie/list?api_key=${ this.apiKey }`,
+    );
 
     return response.json();
   };
 
   getPopularMovies = async (page = 1) => {
-    const response = await fetch(`${ this.apiBaseUrl }/movie/popular?api_key=${ this.apiKey }&page=${ page }`);
+    const response = await fetch(
+      `${ this.apiBaseUrl }/movie/popular?api_key=${ this.apiKey }&page=${ page }`,
+    );
 
     return response.json();
   };
