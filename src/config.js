d1351bea6f3c7a47b282c5435a8f8dc65d8561af
Vladyslav Havryliuk <vladyslavhavryliuk@Vladyslavs-MacBook-Pro.local>
2024-03-05 02:54:31 +0200

    change cover placeholder URL

--- src/config.js
+++ src/config.js
@@ -2,5 +2,4 @@ export const API_KEY = '33561cf194ba1923fda94b17d5d54eee';
 
 export const IMAGES_PATH = 'https://image.tmdb.org/t/p';
 
-export const COVER_PLACEHOLDER = 'https://via.placeholder.com/300x450.png?text=No+Cover';
-
+export const COVER_PLACEHOLDER = 'https://placehold.co/300x450.png?text=No+Cover&font=roboto';
