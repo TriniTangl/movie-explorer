68a4eaf87fa727d2afc16495bf44e9612abe25bf
Vladyslav Havryliuk <vladyslavhavryliuk@Vladyslavs-MacBook-Pro.local>
2024-03-07 19:33:56 +0200

    add API errors mesages enum

---
+++ src/constants/enums.js
@@ -0,0 +1,6 @@
+export const API_ERROR_MESSAGES = {
+  SEARCH: 'Failed to search for movies. Please reload the page or try again later.',
+  GENRES: 'Failed to get list of genres. Please reload the page or try again later.',
+  POPULAR: 'Failed to get list of popular movies. Please reload the page or try again later.',
+  DETAILS: 'Failed to get information about selected movie. Please reload the page or try again later.',
+};
